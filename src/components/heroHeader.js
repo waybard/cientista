4ef2b25b145d39bec9985c2ec09a53e5d6c6d5e7
waybard <waybard@gmail.com>
2020-09-01 21:25:22 -0300

    Update heroHeader.js

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -15,49 +15,14 @@ export default () => (
       }
     `}
     render={data => (
-      <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: 'https://htmlcolorcodes.com/assets/images/html-color-codes-color-tutorials-hero-00e10b1f.jpg)',
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`
-      }}>
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column'
-        }}>
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              'rgba(0, 0, 0, 0.75) 0.5rem 0px 0px, rgba(0, 0, 0, 0.75) -0.5rem 0px 0px',
-            backgroundColor: 'rgba(0, 0, 0, 0.75)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em'
-          }}>
-          {data.site.siteMetadata.home.title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgba(0, 0, 0, 0.75) 0.5rem 0px 0px, rgba(0, 0, 0, 0.75) -0.5rem 0px 0px',
-            backgroundColor: 'rgba(0, 0, 0, 0.75)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em'
-          }}>
-          { data.site.siteMetadata.home.description}
-        </h3>
+      <div className="hero-header">
+        <div className="headline">{data.site.siteMetadata.home.title}</div>
+        <div
+          className="primary-content"
+          dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
+        />
+        <Link to='/contact' className="button -primary">Get in touch &rarr;</Link>
       </div>
-    </div>
     )}
   />
 )
